38ba2ec587f2ebf73856757267be466506f307fb
Mikazuki Tomoko <142188839+Yesord@users.noreply.github.com>
2024-04-15 09:26:34 +0800

    wifi part
    AT_cmdset.h push

---
+++ Code/Core0/Core/Inc/AT_cmdset.h
@@ -0,0 +1,30 @@
+# ifndef _AT_CMDSET_H_
+# define _AT_CMDSET_H_
+
+/****************************************
+参考AT指令集
+https://github.com/CQUPTLei/ESP8266
+****************************************/
+
+//基础
+#define AT_Test "AT\r\n" //测试AT指令
+#define AT_RST "AT+RST\r\n" //重启模块
+#define AT_GMR "AT+GMR\r\n" //获取固件版本号
+#define AT_SYSMSG "AT+SYSMSG\r\n" //获取系统信息
+#define AT_CWMODE "AT+CWMODE=1\r\n" //设置WIFI模式
+
+//WIFI
+#define AT_WIFI_INIT "AT+CWINT\r\n" //初始化WIFI
+#define AT_WIFI_MODE "AT+CWMODE?\r\n" //查询WIFI模式
+#define AT_WIFI_CONNECT "AT+CWJAP=\"SSID\",\"PASSWORD\"\r\n" //连接WIFI
+#define AT_WIFI_MODE_STATION "AT+CWMODE=1\r\n" //设置WIFI模式
+#define AT_WIFI_MODE_SOFTAP "AT+CWMODE=2\r\n" //设置WIFI模式
+#define AT_WIFI_MODE_SOFTAP_STATION "AT+CWMODE=3\r\n" //设置WIFI模式
+#define AT_WIFI_STATE "AT+CWSTATE\r\n" //查询WIFI状态
+#define AT_WIFI_SCAN "AT+CWLAP\r\n" //扫描WIFI
+#define AT_WIFI_DISCONNECT "AT+CWQAP\r\n" //断开WIFI
+#define AT_WIFI_IP "AT+CIPSTA?\r\n" //查询IP地址
+#define AT_WIFI_TRANSMISSION_MODE "AT+CIPMODE=1\r\n" //设置透传模式
+#define AT_WIFI_SERVER "AT+CIPSERVER=1,8080\r\n" //开启服务器
+
+# endif
--- Code/Core0/Core/Src/wifi.c
+++ Code/Core0/Core/Src/wifi.c
@@ -1,6 +1,66 @@
 #include "wifi.h"
-
+#include "AT_cmdset.h"
 #include "main.h"
+
+void wifi_server_init(void)
+{
+    // Initialize the wifi module
+    printf(AT_Test); //检测设备是否在线
+    printf(AT_RST); //让Wifi模块重启的命令
+    printf(AT_GMR); //获取固件版本号
+    printf(AT_SYSMSG); //获取系统信息
+    printf(AT_WIFI_MODE_SOFTAP); //设置WIFI模式
+    printf(AT_RST); //让Wifi模块重启的命令
+    printf(AT_WIFI_SET); //设置WIFI
+    printf(AT_WIFI_SERVER); //开启服务器
+    
+}
+
+void wifi_client_init(void)
+{
+    // Initialize the wifi module
+    printf(AT_Test); //检测设备是否在线
+    printf(AT_RST); //让Wifi模块重启的命令
+    printf(AT_GMR); //获取固件版本号
+    printf(AT_SYSMSG); //获取系统信息
+    printf(AT_WIFI_MODE_STATION); //设置WIFI模式
+    printf(AT_RST); //让Wifi模块重启的命令
+
+}
+
+void wifi_SCAN(void)
+{
+    // Connect to the wifi network
+    printf(AT_WIFI_SCAN); //扫描WIFI
+}
+
+
+void wifi_connect(char* ssid, char* password)
+{
+    // Connect to the wifi network
+    printf("AT+CWJAP=\"%s\",\"%s\"\r\n",ssid,password); //连接WIFI
+}
+
+void wifi_TCP_connect(char* ip, char* port)
+{
+    // Connect to the wifi network
+    printf("AT+CIPSTART=\"TCP\",\"%s\",%s\r\n",ip,port); //连接TCP
+}
+
+void wifi_send_data(uint8_t *data, uint16_t len)
+{
+    // Send data over wifi
+    printf("AT+CIPSEND=0,%d\r\n",len); //发送数据
+    //可能需要延迟
+    for(int i=0; i<len; i++)
+    {
+        putchar(data[i]); //发送数据
+    }
+    putchar(26); //发送结束符
+}
+
+
+
 
 
 
